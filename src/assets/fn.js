ab88b182b53d858a424df1ef8b3afb0fc30b32b7
50048873 <50048873@qq.com>
2022-03-04 10:06:16 +0800

    continue fn.js

--- src/assets/fn.js
+++ src/assets/fn.js
@@ -1,13 +1,6 @@
 /* eslint-disable */
-const factorial = (function fn (num) {
-    if (num <= 1) {
-        return 1
-    } else {
-        return num * fn(num - 1)
-    }
-})
-
 export const getCellSpanProperty = function (cell) {
+    // console.log(cell)
     let rowspan = cell.getAttribute('rowspan')
     let colspan = cell.getAttribute('colspan')
     rowspan = rowspan ? rowspan * 1 : 1
@@ -64,31 +57,31 @@ export const getIndexEnd = (function fn (rows, rowStart, rowEnd, colStart, colEn
     }
 })
 
-export const getPreTrCell = (rows, i, j) => {
+export const getPreTrCellInfo = (rows, i, j) => {
     if (i > 0) {
         let m = i - 1
         let preTrCell = rows[m].children[j]
-        // const rowspan = preTrCell.getAttribute('rowspan')
         while (preTrCell && preTrCell.style.display === 'none') {
             m--
             preTrCell = rows[m].children[j]
         }
-        return preTrCell
+        return {
+            preTrCell,
+            preTrIndex: m,
+        }
     }
 }
 
-export const getPreCell = (cell, j) => {
-    let preCell = cell.previousElementSibling
-    while (preCell && preCell.style.display === 'none') {
-        preCell = preCell.previousElementSibling
+export const getPreCellInfo = (cell, j) => {
+    if (!cell.previousElementSibling) {
+        return {
+            preCell: cell,
+            preCellIndex: j,
+        }
     }
-    return preCell
-}
-
-export const getPreCellMaxCol = (cell, j) => {
     let preCell = cell.previousElementSibling
     let k = j - 1
-    while (preCell && preCell.style.display === 'none') {
+    while (k > 0 && preCell && preCell.style.display === 'none') {
         preCell = preCell.previousElementSibling
         k--
     }
@@ -96,10 +89,13 @@ export const getPreCellMaxCol = (cell, j) => {
         const {colspan} = getCellSpanProperty(preCell)
         k = k + (colspan - 1)
     } 
-    return k
+    return {
+        preCell,
+        preCellIndex: k,
+    }
 }
 
-export const getIndexStart = (function fn (rows, rowStart, rowEnd, colStart, colEnd) {
+export const getIndexStart = (rows, rowStart, rowEnd, colStart, colEnd) => {
     for (let i = rowEnd; i >= rowStart; i--) {
         const {children} = rows[i]
         for (let j = colEnd; j >= colStart; j--) {
@@ -109,25 +105,36 @@ export const getIndexStart = (function fn (rows, rowStart, rowEnd, colStart, col
                 // 仅行合并
                 // 1、左边单元格不为none
                 // 2、左边单元格为none
-                const preTrCell = getPreTrCell(rows, i, j)
-                const preCell = getPreCell(preCell)
-                const preCellMaxCol = getPreCellMaxCol(cell, j)
-                console.log(preTrCell, preCellMaxCol)
-
-                const {rowspan} = getCellSpanProperty(preTrCell)
-                if (rowspan > 1) {
-                    if (preCell) {
-
-                    }
-                }
 
                 // 仅列合并
                 // 1、上面单元格不为none
                 // 2、上面单元格为none
 
+
                 // 行列合并(上面单元格与同行上一个单元格都为none)
                 // 1、左边不影响当前
                 // 2、左边与当前为一体
+
+                const {preTrCell, preTrIndex} = getPreTrCellInfo(rows, i, j)
+                const {preCell, preCellIndex} = getPreCellInfo(cell, j)
+                const {rowspan: preTrRowspan} = getCellSpanProperty(preTrCell)
+                const {colspan} = getCellSpanProperty(preCell)
+                // console.log(preTrRowspan, colspan)
+                if (preTrRowspan > 1 && j > preCellIndex) { // 仅行合并
+                    const diff = i - preTrIndex
+                    const newRowStart = i - diff
+                    if (newRowStart < rowStart && newRowStart >= 0) {
+                        rowStart = newRowStart
+                    }
+                    // console.log(i, j, ':', preTrIndex, diff, rowStart)
+                } else if (colspan > 1 && j <= preCellIndex) { // 仅列合并
+                    const c = j - (colspan - 1)
+                    if (c < colStart) {
+                        colStart = c > 0 ? c : 0
+                    }
+                } /*else if () { // 行列合并
+
+                }*/
             }
         } 
     }
@@ -136,46 +143,15 @@ export const getIndexStart = (function fn (rows, rowStart, rowEnd, colStart, col
         rowStart,
         colStart,
     }
-})
+}
 
-
-const temp = () => {
-    // 列
-    /*if (colspan > 1) {
-        const _colEnd = j + (colspan - 1)
-        if (_colEnd > colEnd) {
-            colEnd = _colEnd
-            return fn(rows, rowStart, rowEnd, colStart, colEnd)
+export const isRect = (rows) => {
+    const trLen = rows.length
+    for (let i = 0; i <= trlen; i++) {
+        const {children} = rows[i]
+        for (let j = colEnd; j >= colStart; j--) {
+            const cell = children[j]
+            // console.log(i, j, cell)
         } 
-    }*/
-
-    /*if (i === 0) continue
-    let m = i - 1
-    let preTrCell = rows[m].children[j]
-    console.log(preTrCell)
-    while (preTrCell && preTrCell.style.display === 'none') {
-        m--
-        preTrCell = rows[m].children[j].previousElementSibling
     }
-    // console.log(preTrCell)
-    const {
-        rowspan: preTrRowspan, 
-    } = getCellSpanProperty(preTrCell)
-
-    let preCell = cell.previousElementSibling
-    let k = j - 1
-    while (preCell && preCell.style.display === 'none') {
-        preCell = preCell.previousElementSibling
-        k--
-    }
-    if (preCell) {
-        const {colspan} = getCellSpanProperty(preCell)
-        k = k + (colspan - 1)
-    } 
-    // console.log(k, m, rows[m], preTrCell, preTrRowspan)
-    if (k < j && preTrRowspan > 1) {
-        if (m < rowStart) {
-            rowStart = m
-        }
-    }*/
 }
